a68017da58d82ba36cd0ae8868591a6c5256dd6d
Gino Marckx <gino.marckx@xodiac.ca>
2020-10-06 19:55:42 -0400

    Enable feature service mock to be created without arguments

--- src/app/feature.service.mock.ts
+++ src/app/feature.service.mock.ts
@@ -1,6 +1,6 @@
 import {FeatureService} from "./feature.service";
 
-export function withFeatures(features: string[]): FeatureService {
+export function withFeatures(features: string[] = []): FeatureService {
     return {
         isOn: jest.fn(f => features && features.includes(f))
     } as FeatureService
