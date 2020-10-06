10ac5620fed00d2c552f28219cc7121424f51a43
Gino Marckx <gino.marckx@xodiac.ca>
2020-10-06 19:03:58 -0400

    Add FeatureService mock for ease of mocking.

---
+++ src/app/feature.service.mock.ts
@@ -0,0 +1,7 @@
+import {FeatureService} from "./feature.service";
+
+export function withFeatures(features: string[]): FeatureService {
+    return {
+        isOn: jest.fn(f => features && features.includes(f))
+    } as FeatureService
+}
--- src/app/feature.service.spec.ts
+++ src/app/feature.service.spec.ts
@@ -1,22 +1,42 @@
-import { FeatureService } from './feature.service';
+import {FeatureService} from './feature.service';
+import {withFeatures} from "./feature.service.mock";
 
-import { FeatureFlags } from './feature.flags'
+import {FeatureFlags} from './feature.flags'
 jest.mock('./feature.flags')
 
 describe('FeatureService', () => {
 
   it('is off for unknown feature', () => {
-    expect(new FeatureService().isOn('unknown.feature')).toBe(false)
+    expect(new FeatureService().isOn('unknown feature')).toBe(false)
   })
 
   it('is on for enabled feature', () => {
-    FeatureFlags['enabled.feature'] = true
-    expect(new FeatureService().isOn('enabled.feature')).toBe(true)
+    FeatureFlags['enabled feature'] = true
+    expect(new FeatureService().isOn('enabled feature')).toBe(true)
   })
 
   it('is off for disabled feature', () => {
-    FeatureFlags['disabled.feature'] = false
-    expect(new FeatureService().isOn('disabled.feature')).toBe(false)
+    FeatureFlags['disabled feature'] = false
+    expect(new FeatureService().isOn('disabled feature')).toBe(false)
   })
 
 })
+
+describe('FeatureServiceMock.withFeatures', () => {
+
+  it('is off for unspecified feature', () => {
+    expect(withFeatures([]).isOn('unspecified feature')).toBe(false)
+  })
+
+  it('is on for specified feature', () => {
+    expect(withFeatures(['specified feature']).isOn('specified feature')).toBe(true)
+  })
+
+  it('is on for multiple specified features', () => {
+    let serviceMock = withFeatures(['specified feature', 'another specified feature']);
+    expect(serviceMock.isOn('specified feature')).toBe(true)
+    expect(serviceMock.isOn('another specified feature')).toBe(true)
+  })
+
+})
+
